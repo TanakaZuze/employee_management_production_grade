14425f383a7bf571f3c32f95b188992d4a62ab5c
TanakaZuze <99540354+TanakaZuze@users.noreply.github.com>
2025-05-14 20:53:56 +0200

    init commt
    initial commit without configuration

---
+++ src/main/java/com/production_grade/employee_management/EmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.production_grade.employee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/production_grade/employee_management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.production_grade.employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
